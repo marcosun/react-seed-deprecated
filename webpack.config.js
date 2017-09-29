2ccce1d41132d8dd561f0a59d1c075b6922d1d96
Marco Sun <sunhq@ibuscloud.com>
2017-09-29 16:45:16 +0800

    import css loader and style loader for loading css

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,37 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
-      { test: /\.js|\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
-  }
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true,
+              convertToAbsoluteUrls: true,
+              hmr: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              camelCase: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
