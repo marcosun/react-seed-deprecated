da3fe06c80ce0007ddb162e4a8782818f041303d
Marco Sun <sunhq@ibuscloud.com>
2017-10-10 17:29:01 +0800

    extract runtime

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -50,6 +50,11 @@ module.exports = merge.smartStrategy(
     new webpack.NamedModulesPlugin(), // Prints more readable module names in the browser console on HMR updates
 
     new webpack.NoEmitOnErrorsPlugin(), // Do not emit compiled assets that include errors
+
+    new webpack.optimize.CommonsChunkPlugin({ // Bundle runtime into a seperate file
+      name: 'runtime',
+      minChunks: Infinity,
+    }),
   ],
 
   devServer: {
