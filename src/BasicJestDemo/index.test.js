8156a31e2e77aa6c29a9c62cace4b2068dfa8c8d
Marco Sun <sunhq@ibuscloud.com>
2017-10-26 16:17:22 +0800

    move test file under the same folder of source code

--- src/BasicJestDemo/index.test.js
+++ src/BasicJestDemo/index.test.js
@@ -2,7 +2,7 @@
   * This demo tests a function in src/BasicJestDemo/index
 */
 
-import sum from '../../src/BasicJestDemo/index';
+import sum from './index';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
