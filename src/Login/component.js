c7948e1b0af8318b919ae455e37b002358c09731
Marco Sun <sunhq@ibuscloud.com>
2017-11-01 16:03:49 +0800

    disable css modules, try css-in-js instead

--- src/Login/component.js
+++ src/Login/component.js
@@ -5,9 +5,62 @@
  * @requires prop-types
  */
 import React from 'react';
-import {string, func} from 'prop-types';
+import {string, func, object} from 'prop-types';
 
-import Styles from './style.css';
+import Button from 'material-ui/Button';
+import {withStyles} from 'material-ui/styles';
+// Css module is disabled, instead I am using css-in-js
+// This approach gives css full functionality from javascript
+// import Styles from './style.css';
+
+const styles = (theme) => ({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    background: {
+      position: 'center',
+      size: '100vw 100vh',
+      repeat: 'no-repeat',
+      image: `url(${require('./traffic.gif')})`,
+    },
+  },
+  form: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate3d(-50%,-50%,0)',
+    width: '300px',
+    textAlign: 'center',
+    color: 'white',
+  },
+  subTitle: {
+    '& span': {
+      '&:first-child': {
+        color: 'rgb(239,65,53)',
+      },
+      '&:nth-child(2)': {
+        color: 'white',
+      },
+      '&:last-child': {
+        color: 'rgb(0,85,164)',
+      },
+    },
+  },
+  rowWrapper: {
+    justifyContent: 'center',
+    margin: '10px',
+  },
+  splitWrapper: {
+    '& label': {
+      flex: '0 0 70px',
+      textAlign: 'left',
+    },
+    '& input': {
+      flex: '0 0 150px',
+      width: '150px',
+    },
+  },
+});
 
 /**
  * Login Component
@@ -19,7 +72,11 @@ import Styles from './style.css';
  * @param {function} props.onPasswordTyping - Password typing callback
  * @param {function} props.onSubmit - Submit button click callback
  */
-export default class Component extends React.Component {
+class Component extends React.Component {
+  state = {
+    loading: false,
+  };
+
   /**
    * Props validation
    */
@@ -29,6 +86,7 @@ export default class Component extends React.Component {
     onUsernameTyping: func.isRequired,
     onPasswordTyping: func.isRequired,
     onSubmit: func.isRequired,
+    classes: object.isRequired,
   };
 
   /**
@@ -36,40 +94,57 @@ export default class Component extends React.Component {
    * @return {Component}
    */
   render() {
+    const {
+      classes,
+      username,
+      password,
+      onUsernameTyping,
+      onPasswordTyping,
+      onSubmit,
+    } = this.props;
+    const usernamePasswordClassname = `
+      row
+      ${classes.rowWrapper}
+      ${classes.splitWrapper}
+    `;
+    const submitClassname = `row ${classes.rowWrapper}`;
+
     return (
-      <div className={Styles.loginContainer}>
-        <form className={Styles.formContainer} onSubmit={this.props.onSubmit}>
+      <div className={classes.root}>
+        <form className={classes.form} onSubmit={onSubmit}>
           <h1>公交云</h1>
-          <h3 className={Styles.subTitle}>
+          <h3 className={classes.subTitle}>
             <span>i</span>
             <span>Bus</span>
             <span>Cloud</span>
           </h3>
-          <div className={`row ${Styles.username}`}>
+          <div className={usernamePasswordClassname}>
             <label>
               用户名：
             </label>
             <input type='text'
               name='username'
-              value={this.props.username}
-              onChange={this.props.onUsernameTyping}
+              value={username}
+              onChange={onUsernameTyping}
             />
           </div>
-          <div className={`row ${Styles.password}`}>
+          <div className={usernamePasswordClassname}>
             <label>
               密码：
             </label>
             <input type='password'
               name='password'
-              value={this.props.password}
-              onChange={this.props.onPasswordTyping}
+              value={password}
+              onChange={onPasswordTyping}
             />
           </div>
-          <div className={`row ${Styles.submit}`}>
-            <button type='submit'>登录</button>
+          <div className={submitClassname}>
+            <Button raised color='accent' >登录</Button>
           </div>
         </form>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(Component);
