6ce5aad4dc541d245e3cdd746baf959c46beac75
Marco Sun <sunhq@ibuscloud.com>
2017-11-27 16:36:10 +0800

    wrap form in redux form to take advantage of its rich features such as validation and form field state management

--- src/Login/component.js
+++ src/Login/component.js
@@ -3,15 +3,51 @@
  * @module Login/Component
  * @requires react
  * @requires prop-types
+ * @requires material-ui
+ * @requires redux-form
  */
 import React from 'react';
-import {string, func, object, bool} from 'prop-types';
-
+import {func, object, bool} from 'prop-types';
 import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
 import {withStyles} from 'material-ui/styles';
+import {Field, reduxForm} from 'redux-form';
 // Css module is disabled, instead I am using css-in-js
 // This approach gives css full functionality from javascript
 // import Styles from './style.css';
+
+// Form validate function provided to redux form
+const validate = (values) => {
+  const errors = {};
+  const requiredFields = [
+    'username',
+    'password',
+  ];
+
+  requiredFields.forEach((field) => {
+    if (!values[field]) {
+      errors[field] = '不得为空';
+    }
+  });
+
+  return errors;
+};
+
+// The following function returns TextField of material-ui
+// This wrapper function is required by redux form
+// Consider creating a TextField wrapper function to use in the entire project
+const renderTextField = ({
+  input,
+  meta: {touched, error},
+  ...custom
+}) => (
+  <TextField
+    helperText={touched && error}
+    error={touched && error && true}
+    {...input}
+    {...custom}
+  />
+);
 
 const styles = (theme) => ({
   root: {
@@ -50,15 +86,8 @@ const styles = (theme) => ({
     justifyContent: 'center',
     margin: '10px',
   },
-  splitWrapper: {
-    '& label': {
-      flex: '0 0 70px',
-      textAlign: 'left',
-    },
-    '& input': {
-      flex: '0 0 150px',
-      width: '150px',
-    },
+  textField: {
+    backgroundColor: 'white',
   },
 });
 
@@ -66,11 +95,7 @@ const styles = (theme) => ({
  * Login Component
  * @extends {Component}
  * @param {Object} props
- * @param {string} props.username - Login username
- * @param {string} props.password - Login password
  * @param {boolean} props.isAuthenticating - Login status
- * @param {function} props.onUsernameTyping - Username typing callback
- * @param {function} props.onPasswordTyping - Password typing callback
  * @param {function} props.onSubmit - Submit button click callback
  */
 class Component extends React.Component {
@@ -82,13 +107,13 @@ class Component extends React.Component {
    * Props validation
    */
   static propTypes = {
-    username: string.isRequired,
-    password: string.isRequired,
     isAuthenticating: bool.isRequired,
-    onUsernameTyping: func.isRequired,
-    onPasswordTyping: func.isRequired,
-    onSubmit: func.isRequired,
     classes: object.isRequired,
+    onSubmit: func.isRequired,
+    handleSubmit: func.isRequired,
+    pristine: bool.isRequired,
+    reset: func.isRequired,
+    submitting: bool.isRequired,
   };
 
   /**
@@ -98,52 +123,56 @@ class Component extends React.Component {
   render() {
     const {
       classes,
-      username,
-      password,
       isAuthenticating,
-      onUsernameTyping,
-      onPasswordTyping,
       onSubmit,
+      // The following four props are passed in by reduxForm
+      // 1. handleSubmit should bind to onSubmit callback of the form
+      // 2. pristine is a boolean value indicating
+      // if all fields of the form are in their original state
+      // 3. reset function resets all fields
+      // 4. submitting is a boolean value
+      handleSubmit,
+      pristine,
+      reset,
+      submitting,
     } = this.props;
-    const usernamePasswordClassname = `
-      row
-      ${classes.rowWrapper}
-      ${classes.splitWrapper}
-    `;
     const submitClassname = `row ${classes.rowWrapper}`;
     const alert = isAuthenticating ? <div>isAuthenticating</div> : null;
-
     return (
       <div className={classes.root}>
-        <form className={classes.form} onSubmit={onSubmit}>
+        <form className={classes.form} onSubmit={handleSubmit}>
           <h1>公交云</h1>
           <h3 className={classes.subTitle}>
             <span>i</span>
             <span>Bus</span>
             <span>Cloud</span>
           </h3>
-          <div className={usernamePasswordClassname}>
-            <label>
-              用户名：
-            </label>
-            <input type='text'
-              name='username'
-              value={username}
-              onChange={onUsernameTyping}
-            />
-          </div>
-          <div className={usernamePasswordClassname}>
-            <label>
-              密码：
-            </label>
-            <input type='password'
-              name='password'
-              value={password}
-              onChange={onPasswordTyping}
-            />
-          </div>
+          <Field
+            component={renderTextField}
+            type='text'
+            name='username'
+            InputClassName={classes.textField}
+            margin='normal'
+            placeholder='用户名...'
+            autoFocus
+          />
+          <Field
+            component={renderTextField}
+            type='password'
+            name='password'
+            InputClassName={classes.textField}
+            margin='normal'
+            placeholder='密码...'
+          />
           <div className={submitClassname}>
-            <Button raised color='accent' type='submit'>登录</Button>
+            <Button
+              type='submit'
+              raised
+              color='accent'
+              disabled={submitting}
+            >
+              登录
+            </Button>
           </div>
           {alert}
         </form>
@@ -152,4 +181,9 @@ class Component extends React.Component {
   }
 }
 
-export default withStyles(styles)(Component);
+const LoginPageLoginForm = withStyles(styles)(Component);
+
+export default reduxForm({
+  form: 'LoginPageLoginForm', // a unique identifier for this form
+  validate,
+})(LoginPageLoginForm);
